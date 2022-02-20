403e6368189266e53a92ce224c509a9f887a9a99
hitmaizer <jose.sousalves@gmail.com>
2022-02-20 12:53:31 +0000

    added color variables to theme file

---
+++ src/theme/theme.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+export default {
+    colors: {
+        white: "#ffffff",
+        gray: "#6E707A",
+        offGray: "#E7E7EB",
+        darkGray:"#A09FB1",
+        grayBlue: "#100E1D",
+        darkBlue: "#1E213A",
+        lightBlue: "#3C47E9",
+        offWhite: "#616475",
+        yellow: "#FFEC65",
+        mediumGray: "#88869D",
+        purpleBlue: "#585676",
+        whiteGray: "#E7E7EB", 
+    }
+}
