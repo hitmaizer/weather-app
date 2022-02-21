9bf230c89076c8776a526c34da5e5cbf5dd22c29
hitmaizer <jose.sousalves@gmail.com>
2022-02-21 10:56:04 +0000

    import + render sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { ThemeProvider } from "styled-components";
+import Sidebar from './components/Sidebar';
 import Theme from './theme/theme'
 
 export default function App() {
     return (
         <div className="page__wrapper flex-row">
             <ThemeProvider theme={Theme}>
-              
+              <Sidebar />
             </ThemeProvider>
         </div>
     )
