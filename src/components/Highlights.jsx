e35c84532d0db9122d8fd198945ffb56c2c633cb
hitmaizer <jose.sousalves@gmail.com>
2022-02-21 13:02:08 +0000

    jsx markup for highlight component + imports

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import WindStatus from '../elements/WindStatus'
+import Humidity from '../elements/Humidity'
+import Visibility from '../elements/Visibility'
+import AirPressure from '../elements/AirPressure'
 
 export default function Highlights() {
     return  (
-        <h1>Isto vao ser os highlights</h1>
+        <div className="highlights__wrapper flex-col">
+            <h1 className="highlights__title">Today's Highlights</h1>
+            <div className="highlights__grid">
+                <WindStatus />
+                <Humidity />
+                <Visibility />
+                <AirPressure />
+            </div>
+
+        </div>
     )
 }
