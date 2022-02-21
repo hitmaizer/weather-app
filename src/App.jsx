71bfc58d5c3e8fe1f95087733dea529ece4c9875
hitmaizer <jose.sousalves@gmail.com>
2022-02-21 16:28:13 +0000

    started drawer logic + markup

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,37 @@ import { ThemeProvider } from "styled-components";
 import Main from './components/Main';
 import Sidebar from './components/Sidebar';
 import Theme from './theme/theme'
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+
 
 export default function App() {
+    const [state, setState] = React.useState(false)
+
+    //your first function
+    function toggleDrawer(open) {
+        //your secound function
+              return function (event) {
+                  return setState(open)
+              }
+          } 
+
     return (
         <div className="page__wrapper flex-row">
             <ThemeProvider theme={Theme}>
-              <Sidebar />
+              <Sidebar toggleDrawer={toggleDrawer}/>
+              <Drawer 
+              anchor={'left'}
+              open={state}
+              onClose={toggleDrawer(false)}
+              />
               <Main />
             </ThemeProvider>
         </div>
