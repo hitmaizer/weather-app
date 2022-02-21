7f473f5bba0df4b2221ae4ea100be56c79c29d52
hitmaizer <jose.sousalves@gmail.com>
2022-02-21 16:09:26 +0000

    created progress bar with styled component

---
+++ src/elements/StyledProgressBar.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export default styled.div`
+    background-color: ${(props) => props.theme.colors.yellow};
+    width: 80%;
+    height: 8px;
+    border-radius: 80px;
+`;
