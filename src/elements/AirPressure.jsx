adbf530a08c775f751a150c09472b8e59d4d4024
hitmaizer <jose.sousalves@gmail.com>
2022-02-21 16:09:46 +0000

    added generic class for styling

--- src/elements/AirPressure.jsx
+++ src/elements/AirPressure.jsx
@@ -3,11 +3,11 @@ import StyledCard from './StyledCard'
 
 export default function AirPressure() {
     return (
-        <StyledCard className="airpressure__wrapper flex-col">
-            <h5 className="airpressure__title">Air Pressure</h5>
-            <div className="airpressure__details flex-row">
-                <h1 className="details__number">998</h1>
-                <h3 className="details__type">mb</h3>
+        <StyledCard className="airpressure__wrapper flex-col highlight__card">
+            <h5 className="airpressure__title highlight__title">Air Pressure</h5>
+            <div className="highlights__main flex-row">
+                <h1 className="details__number highlight__detail">998</h1>
+                <h3 className="details__type highlight__type">mb</h3>
             </div>
 
         </StyledCard>
