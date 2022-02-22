a32cc99a54a8421283110918d0c8003645fcf2da
hitmaizer <jose.sousalves@gmail.com>
2022-02-22 22:47:35 +0000

    visibility dynamic render

--- src/elements/Visibility.jsx
+++ src/elements/Visibility.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import StyledCard from './StyledCard'
 
-export default function Visibility() {
+export default function Visibility(props) {
     return (
         <StyledCard className="visibility__wrapper flex-col highlight__card">
             <h5 className="visibility__title highlight__title">Visibility</h5>
             <div className="highlights__main flex-row">
-                <h1 className="details__number highlight__detail">6,4</h1>
+                <h1 className="details__number highlight__detail">{Math.floor(props.weatherDetails.visibility)}</h1>
                 <h3 className="details__type highlight__type">miles</h3>
             </div>
 
