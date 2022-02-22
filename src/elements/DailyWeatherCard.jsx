de3007314af784e967b52b7a8cb67ca21ffabd25
hitmaizer <jose.sousalves@gmail.com>
2022-02-22 22:39:39 +0000

    title date rendering from data

--- src/elements/DailyWeatherCard.jsx
+++ src/elements/DailyWeatherCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import StyledCard from './StyledCard'
 
-export default function DailyWeatherCard() {
+export default function DailyWeatherCard(props) {
+    console.log(props.item)
     return (
         <StyledCard className="forecast__card flex-col">
-            <p className="daily__date">Tomorrow</p>
+            <p className="daily__date">{props.details.applicable_date}</p>
             <img src="images/HeavyRain.png" alt="" className="daily__img" />
             <div className="daily__minmax flex-row">
                 <p className="minmax max">16ºC</p>
