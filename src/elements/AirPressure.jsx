4dcb8da368f755556d213054324165e18dd0c312
hitmaizer <jose.sousalves@gmail.com>
2022-02-22 22:47:05 +0000

    airpressure dynamic render

--- src/elements/AirPressure.jsx
+++ src/elements/AirPressure.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import StyledCard from './StyledCard'
 
-export default function AirPressure() {
+export default function AirPressure(props) {
     return (
         <StyledCard className="airpressure__wrapper flex-col highlight__card">
             <h5 className="airpressure__title highlight__title">Air Pressure</h5>
             <div className="highlights__main flex-row">
-                <h1 className="details__number highlight__detail">998</h1>
+                <h1 className="details__number highlight__detail">{props.weatherDetails.air_pressure}</h1>
                 <h3 className="details__type highlight__type">mb</h3>
             </div>
 
