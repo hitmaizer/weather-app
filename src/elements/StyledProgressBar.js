7767cfca4ece2488757505a6ba3aa409f5d792d2
hitmaizer <jose.sousalves@gmail.com>
2022-02-22 22:39:20 +0000

    deleted file to make width render from props

--- src/elements/StyledProgressBar.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.div`
-    background-color: ${(props) => props.theme.colors.yellow};
-    width: 80%;
-    height: 8px;
-    border-radius: 80px;
-`;
