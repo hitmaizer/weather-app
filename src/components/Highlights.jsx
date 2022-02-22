1a2b3e37b575b8b1c5df73d338436ca98be3f0eb
hitmaizer <jose.sousalves@gmail.com>
2022-02-22 22:38:32 +0000

    props sent to highlights cards

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -4,15 +4,15 @@ import Humidity from '../elements/Humidity'
 import Visibility from '../elements/Visibility'
 import AirPressure from '../elements/AirPressure'
 
-export default function Highlights() {
+export default function Highlights(props) {
     return  (
         <div className="highlights__wrapper flex-col">
             <h1 className="highlights__title">Today's Highlights</h1>
             <div className="highlights__grid">
-                <WindStatus />
-                <Humidity />
-                <Visibility />
-                <AirPressure />
+                <WindStatus weatherDetails={props.weatherDetails} />
+                <Humidity weatherDetails={props.weatherDetails} />
+                <Visibility weatherDetails={props.weatherDetails} />
+                <AirPressure weatherDetails={props.weatherDetails} />
             </div>
 
         </div>
