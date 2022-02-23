69be2eae4f4973abc2a4fc008daa471e4c1a11e9
hitmaizer <jose.sousalves@gmail.com>
2022-02-23 14:47:52 +0000

    onclick event for celsius farh btns

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,8 +6,8 @@ export default function Main(props) {
     return (
         <div className="main__wrapper flex-col">
             <div className="toggle__type flex-row">
-                <button id="celsius" className="toggle__btn toggle__celsius selected" onClick={() => props.setTempFormat("celsius", "celsius")}>ºC</button>
-                <button id="fahrenheit" className="toggle__btn toggle__fahrenheit" onClick={() => props.setTempFormat("fahrenheit", "fahrenheit")}>ºF</button>
+                <button id="celsius" className="toggle__btn toggle__celsius selected" onClick={() => props.setTempFormat("celsius", 0)}>ºC</button>
+                <button id="fahrenheit" className="toggle__btn toggle__fahrenheit" onClick={() => props.setTempFormat("fahrenheit", 1)}>ºF</button>
             </div>
         
         <Forecast 
