901a681255006b2874e1e4d2fe3088e291b88481
hitmaizer <jose.sousalves@gmail.com>
2022-02-23 10:21:16 +0000

    added img container for styling

--- src/elements/DailyWeatherCard.jsx
+++ src/elements/DailyWeatherCard.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import StyledCard from './StyledCard'
 
 export default function DailyWeatherCard(props) {
-    console.log(props.item)
+    
     return (
         <StyledCard className="forecast__card flex-col">
             <p className="daily__date">{props.details.applicable_date}</p>
-            <img src={props.weatherImg} alt="" className="daily__img" />
+            <div className="img__container flex-col">
+                <img src={props.weatherImg} alt="" className="daily__img" />
+            </div>
             <div className="daily__minmax flex-row">
                 <p className="minmax max">{Math.floor(props.details.max_temp)}ºC</p>
                 <p className="minmax min">{Math.floor(props.details.min_temp)}ºC</p>
