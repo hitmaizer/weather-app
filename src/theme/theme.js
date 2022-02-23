6e5270a59961e40f66eb769efe96fbc60e84f708
hitmaizer <jose.sousalves@gmail.com>
2022-02-23 09:41:37 +0000

    added images object with online hosted imgs

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -14,5 +14,20 @@ export default {
         mediumGray: "#88869D",
         purpleBlue: "#585676",
         whiteGray: "#E7E7EB", 
+    },
+
+    images: {
+        clear: "https://i.ibb.co/mNGZRzD/Clear.png",
+        hail: "https://i.ibb.co/d65vJmc/Hail.png",
+        heavyCloud: "https://i.ibb.co/fYFw3kc/Heavy-Cloud.png",
+        heavyRain: "https://i.ibb.co/VMCyS5r/Heavy-Rain.png",
+        lightCloud: "https://i.ibb.co/447Zm1X/Light-Cloud.png",
+        lightRain: "https://i.ibb.co/fpkgYnv/Light-Rain.png",
+        shower: "https://i.ibb.co/Jd8LmRj/Shower.png",
+        sleet: "https://i.ibb.co/gvLR7wL/Sleet.png",
+        snow: "https://i.ibb.co/FnJPtHp/Snow.png",
+        thunderStorm: "https://i.ibb.co/YPhXq33/Thunderstorm.png"
     }
+
+
 }
