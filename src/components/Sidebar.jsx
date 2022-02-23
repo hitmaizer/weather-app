6dcd391468671f76a78617a1d9ae7b7539c6a4d7
hitmaizer <jose.sousalves@gmail.com>
2022-02-23 10:06:09 +0000

    switch case for sidebar weather state image

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,14 +6,52 @@ import { LocationPin } from '@styled-icons/entypo/LocationPin'
 
 export default function Sidebar(props) {
     let today = new Date()
-
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Out", "Nov", "Dec"]
     const weekDay = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
     const day = weekDay[today.getDay()]
     const month = months[today.getMonth()]
     today = day + ", " + today.getDate() + " " + month
     
-    
+    const todayState = props.weatherData[0].weather_state_abbr
+
+    let weatherImg = ""
+    switch(todayState){
+            default:
+            break
+            case "sn":
+                weatherImg = props.theme.images.snow
+            break
+            case "sl":
+                weatherImg = props.theme.images.sleet
+            break
+            case "h":
+                weatherImg = props.theme.images.hail
+            break
+            case "t": 
+                weatherImg = props.theme.images.thunderStorm
+            break
+            case "hr":
+                weatherImg = props.theme.images.heavyRain
+            break
+            case "lr":
+                weatherImg = props.theme.images.lightRain
+            break
+            case "s":
+                weatherImg = props.theme.images.shower
+            break
+            case "hc":
+                weatherImg = props.theme.images.heavyCloud
+            break
+            case "lc":
+                weatherImg = props.theme.images.lightCloud
+            break
+            case "c": 
+                weatherImg = props.theme.images.clear
+            break            
+    }
+
+
+
     return (
         <div className="sidebar__wrapper flex-col">
             <div className="sidebar__top flex-row">
@@ -26,7 +64,7 @@ export default function Sidebar(props) {
                     </span>
                 </button>
             </div>
-        <img src="images/Shower.png" alt="" className="sidebar__weatherimg" />
+        {weatherImg !== "" && <img src={weatherImg} alt="" className="sidebar__weatherimg" />}
         <h1 className="sidebar__weathernumber">{Math.floor(props.weatherData[0].the_temp)}<span className="weathernumber--type">ºC</span></h1>
         <h3 className="sidebar__weathertype">{props.weatherData[0].weather_state_name}</h3>
         <div className="sidebar__details--container flex-col">
