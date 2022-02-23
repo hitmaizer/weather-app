54c7f974abb4be5aaa6ca35199df62b630fd7223
hitmaizer <jose.sousalves@gmail.com>
2022-02-23 10:58:03 +0000

    conditional render wind arrow

--- src/elements/WindStatus.jsx
+++ src/elements/WindStatus.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import StyledCard from './StyledCard'
-import { DiagonalArrowLeftDown } from 'styled-icons/evaicons-solid';
+import { DiagonalArrowLeftDown, DiagonalArrowLeftUp, DiagonalArrowRightDown, DiagonalArrowRightUp, ArrowUpward, ArrowDownward, ArrowBack, ArrowForward } from 'styled-icons/evaicons-solid';
 
 export default function WindStatus(props) {
+    
+
     return (
         <StyledCard className="wind__wrapper flex-col highlight__card">
             <h5 className="wind__title highlight__title">Wind Status</h5>
@@ -11,7 +13,14 @@ export default function WindStatus(props) {
                 <h5 className="wind__type highlight__type">mph</h5>
             </div>
             <div className="wind__direction flex-row">
-                <DiagonalArrowLeftDown size="24px" />
+                {props.weatherDetails.wind_direction_compass === "N" && <ArrowUpward size="24px" />}
+                {props.weatherDetails.wind_direction_compass === "NE" && <DiagonalArrowRightUp size="24px" />}
+                {props.weatherDetails.wind_direction_compass === "E" && <ArrowForward size="24px" />}
+                {props.weatherDetails.wind_direction_compass === "SE" && <DiagonalArrowRightDown size="24px" />}
+                {props.weatherDetails.wind_direction_compass === "S" && <ArrowDownward size="24px" />}
+                {props.weatherDetails.wind_direction_compass === "SW" && <DiagonalArrowLeftDown size="24px" />}
+                {props.weatherDetails.wind_direction_compass === "W" && <ArrowBack size="24px" />}
+                {props.weatherDetails.wind_direction_compass === "NW" && <DiagonalArrowLeftUp size="24px" />} 
                 <h5 className="direction__text">{props.weatherDetails.wind_direction_compass}</h5>
             </div>
 
