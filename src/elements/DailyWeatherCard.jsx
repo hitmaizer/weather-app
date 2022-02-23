db3ef35c91b1d08aa0d3c117c3490a454ea7e198
hitmaizer <jose.sousalves@gmail.com>
2022-02-23 14:46:50 +0000

    dynamic render date as week day with momentjs

--- src/elements/DailyWeatherCard.jsx
+++ src/elements/DailyWeatherCard.jsx
@@ -5,13 +5,13 @@ export default function DailyWeatherCard(props) {
     
     return (
         <StyledCard className="forecast__card flex-col">
-            <p className="daily__date">{props.details.applicable_date}</p>
+            <p className="daily__date">{props.date}</p>
             <div className="img__container flex-col">
                 <img src={props.weatherImg} alt="" className="daily__img" />
             </div>
             <div className="daily__minmax flex-row">
-                <p className="minmax max">{Math.floor(props.details.max_temp)}{props.tempFormat === "celsius" ? "ºC" : "ºF"}</p>
-                <p className="minmax min">{Math.floor(props.details.min_temp)}{props.tempFormat === "celsius" ? "ºC" : "ºF"}</p>
+                <p className="minmax max">{Math.floor(props.details.max_temp)}{props.tempFormat === 0 ? "ºC" : "ºF"}</p>
+                <p className="minmax min">{Math.floor(props.details.min_temp)}{props.tempFormat === 0 ? "ºC" : "ºF"}</p>
             </div>
         </StyledCard>
     )
