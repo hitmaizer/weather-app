3bf5c7008d245439f1125fe183394f332ddabc8a
hitmaizer <jose.sousalves@gmail.com>
2022-02-23 12:11:15 +0000

    fixed annoying styled components error

--- src/elements/Humidity.jsx
+++ src/elements/Humidity.jsx
@@ -3,16 +3,16 @@ import StyledCard from './StyledCard'
 import styled from 'styled-components';
 
 
+const ProgressBar = styled.div`
+background-color: ${(props) => props.theme.colors.yellow};
+width: ${(props) => props.progress}%;
+height: 8px;
+border-radius: 80px;
+`;
 
 
 export default function Humidity(props) {
     
-    const ProgressBar = styled.div`
-    background-color: ${(props) => props.theme.colors.yellow};
-    width: ${props.weatherDetails.humidity}%;
-    height: 8px;
-    border-radius: 80px;
-    `;
 
 
 
@@ -30,7 +30,7 @@ export default function Humidity(props) {
                     <p className="numbers__number">100</p>
                 </div>
                 <div className="progress__container">
-                    <ProgressBar />
+                    <ProgressBar progress={props.weatherDetails.humidity}/>
                 </div>
                 <p className="progressbar__type">%</p>
             </div>
