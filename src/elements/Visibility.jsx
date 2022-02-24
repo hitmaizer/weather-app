ddfcbb6a28d51bda71f446fa7a840a128126f3cd
hitmaizer <jose.sousalves@gmail.com>
2022-02-24 22:04:03 +0000

    fixed conditional rendering using new api fetch

--- src/elements/Visibility.jsx
+++ src/elements/Visibility.jsx
@@ -6,8 +6,8 @@ export default function Visibility(props) {
         <StyledCard className="visibility__wrapper flex-col highlight__card">
             <h5 className="visibility__title highlight__title">Visibility</h5>
             <div className="highlights__main flex-row">
-                <h1 className="details__number highlight__detail">{Math.floor(props.weatherDetails.visibility)}</h1>
-                <h3 className="details__type highlight__type">miles</h3>
+                <h1 className="details__number highlight__detail">{Math.floor(props.weatherDetails.vis_km)}</h1>
+                <h3 className="details__type highlight__type">km</h3>
             </div>
 
         </StyledCard>
