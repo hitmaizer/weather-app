34a7e2ba342f5f9145201b9c97d383b6861d9f07
hitmaizer <jose.sousalves@gmail.com>
2022-02-24 22:03:59 +0000

    fixed conditional rendering using new api fetch

--- src/elements/AirPressure.jsx
+++ src/elements/AirPressure.jsx
@@ -6,7 +6,7 @@ export default function AirPressure(props) {
         <StyledCard className="airpressure__wrapper flex-col highlight__card">
             <h5 className="airpressure__title highlight__title">Air Pressure</h5>
             <div className="highlights__main flex-row">
-                <h1 className="details__number highlight__detail">{props.weatherDetails.air_pressure}</h1>
+                <h1 className="details__number highlight__detail">{props.weatherDetails.pressure_mb}</h1>
                 <h3 className="details__type highlight__type">mb</h3>
             </div>
 
