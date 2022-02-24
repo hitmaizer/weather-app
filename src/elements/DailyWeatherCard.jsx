55072008a4f180fecc21489edd02c573b1267310
hitmaizer <jose.sousalves@gmail.com>
2022-02-24 22:04:08 +0000

    fixed conditional rendering using new api fetch

--- src/elements/DailyWeatherCard.jsx
+++ src/elements/DailyWeatherCard.jsx
@@ -7,11 +7,11 @@ export default function DailyWeatherCard(props) {
         <StyledCard className="forecast__card flex-col">
             <p className="daily__date">{props.date}</p>
             <div className="img__container flex-col">
-                <img src={props.weatherImg} alt="" className="daily__img" />
+                <img src={props.details.day.condition.icon} alt="" className="daily__img" />
             </div>
             <div className="daily__minmax flex-row">
-                <p className="minmax max">{Math.floor(props.details.max_temp)}{props.tempFormat === 0 ? "ºC" : "ºF"}</p>
-                <p className="minmax min">{Math.floor(props.details.min_temp)}{props.tempFormat === 0 ? "ºC" : "ºF"}</p>
+                <p className="minmax max">{props.tempFormat === 0 ? Math.floor(props.details.day.maxtemp_c): Math.floor(props.details.day.maxtemp_f)}{props.tempFormat === 0 ? "ºC" : "ºF"}</p>
+                <p className="minmax min">{props.tempFormat === 0 ? Math.floor(props.details.day.mintemp_c): Math.floor(props.details.day.mintemp_f)}{props.tempFormat === 0 ? "ºC" : "ºF"}</p>
             </div>
         </StyledCard>
     )
