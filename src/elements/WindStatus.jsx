0fb29b7fb2f2326e27249689bbf39e99dd0d5090
hitmaizer <jose.sousalves@gmail.com>
2022-02-24 22:03:39 +0000

    fixed conditional rendering using new api fetch

--- src/elements/WindStatus.jsx
+++ src/elements/WindStatus.jsx
@@ -9,27 +9,27 @@ export default function WindStatus(props) {
         <StyledCard className="wind__wrapper flex-col highlight__card">
             <h5 className="wind__title highlight__title">Wind Status</h5>
             <div className="highlights__main flex-row">
-                <h1 className="wind__number highlight__detail">{Math.floor(props.weatherDetails.wind_speed)}</h1>
-                <h5 className="wind__type highlight__type">mph</h5>
+                <h1 className="wind__number highlight__detail">{Math.floor(props.weatherDetails.wind_kph)}</h1>
+                <h5 className="wind__type highlight__type">kph</h5>
             </div>
             <div className="wind__direction flex-row">
-                {props.weatherDetails.wind_direction_compass === "N" && <ArrowUpward size="24px" />}
-                {props.weatherDetails.wind_direction_compass === "NNE" && <DiagonalArrowRightUp size="24px" />}
-                {props.weatherDetails.wind_direction_compass === "NE" && <DiagonalArrowRightUp size="24px" />}
-                {props.weatherDetails.wind_direction_compass === "ENE" && <DiagonalArrowRightUp size="24px" />}
-                {props.weatherDetails.wind_direction_compass === "E" && <ArrowForward size="24px" />}
-                {props.weatherDetails.wind_direction_compass === "ESE" && <DiagonalArrowRightDown size="24px" />}
-                {props.weatherDetails.wind_direction_compass === "SE" && <DiagonalArrowRightDown size="24px" />}
-                {props.weatherDetails.wind_direction_compass === "SSE" && <DiagonalArrowRightDown size="24px" />}
-                {props.weatherDetails.wind_direction_compass === "S" && <ArrowDownward size="24px" />}
-                {props.weatherDetails.wind_direction_compass === "SSW" && <DiagonalArrowLeftDown size="24px" />}
-                {props.weatherDetails.wind_direction_compass === "SW" && <DiagonalArrowLeftDown size="24px" />}
-                {props.weatherDetails.wind_direction_compass === "WSW" && <DiagonalArrowLeftDown size="24px" />}
-                {props.weatherDetails.wind_direction_compass === "W" && <ArrowBack size="24px" />}
-                {props.weatherDetails.wind_direction_compass === "WNW" && <DiagonalArrowLeftUp size="24px" />} 
-                {props.weatherDetails.wind_direction_compass === "NW" && <DiagonalArrowLeftUp size="24px" />} 
-                {props.weatherDetails.wind_direction_compass === "NNW" && <DiagonalArrowLeftUp size="24px" />} 
-                <h5 className="direction__text">{props.weatherDetails.wind_direction_compass}</h5>
+                {props.weatherDetails.wind_dir === "N" && <ArrowUpward size="24px" />}
+                {props.weatherDetails.wind_dir === "NNE" && <DiagonalArrowRightUp size="24px" />}
+                {props.weatherDetails.wind_dir === "NE" && <DiagonalArrowRightUp size="24px" />}
+                {props.weatherDetails.wind_dir === "ENE" && <DiagonalArrowRightUp size="24px" />}
+                {props.weatherDetails.wind_dir === "E" && <ArrowForward size="24px" />}
+                {props.weatherDetails.wind_dir === "ESE" && <DiagonalArrowRightDown size="24px" />}
+                {props.weatherDetails.wind_dir === "SE" && <DiagonalArrowRightDown size="24px" />}
+                {props.weatherDetails.wind_dir === "SSE" && <DiagonalArrowRightDown size="24px" />}
+                {props.weatherDetails.wind_dir === "S" && <ArrowDownward size="24px" />}
+                {props.weatherDetails.wind_dir === "SSW" && <DiagonalArrowLeftDown size="24px" />}
+                {props.weatherDetails.wind_dir === "SW" && <DiagonalArrowLeftDown size="24px" />}
+                {props.weatherDetails.wind_dir === "WSW" && <DiagonalArrowLeftDown size="24px" />}
+                {props.weatherDetails.wind_dir === "W" && <ArrowBack size="24px" />}
+                {props.weatherDetails.wind_dir === "WNW" && <DiagonalArrowLeftUp size="24px" />} 
+                {props.weatherDetails.wind_dir === "NW" && <DiagonalArrowLeftUp size="24px" />} 
+                {props.weatherDetails.wind_dir === "NNW" && <DiagonalArrowLeftUp size="24px" />} 
+                <h5 className="direction__text">{props.weatherDetails.wind_dir}</h5>
             </div>
 
         </StyledCard>
