94c3973dca6f3feb837718e36d146e269a52c4ff
hitmaizer <jose.sousalves@gmail.com>
2022-02-25 16:31:42 +0000

    props to item element

--- src/elements/PopularCities.jsx
+++ src/elements/PopularCities.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import { Reorder } from "framer-motion"
 import Item from './Item'
 
-const initialItems = ["London", "Berlin", "Paris", "Madrid", "Stockholm", "Helsinki", "Moscow"]
-
 export default function PopularCities(props) {
-    const [items, setItems] = React.useState(initialItems)
-
+    
     return (
-        <Reorder.Group axis="y" onReorder={setItems} values={items}>
-          {items.map((item) => (
+        <Reorder.Group axis="y" onReorder={props.setItems} values={props.items}>
+          {props.items.map((item) => (
             <Item key={item} item={item} handlePopularCities={props.handlePopularCities} />
           ))}
         </Reorder.Group>
