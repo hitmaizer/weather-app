a8e4a5ac1d9981bdff31cf6df8ae53f12b2342eb
hitmaizer <jose.sousalves@gmail.com>
2022-02-25 20:03:50 +0000

    cleanup unused code

--- src/elements/Item.jsx
+++ src/elements/Item.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { ArrowIosForwardOutline } from '@styled-icons/evaicons-outline/ArrowIosForwardOutline'
 import { Reorder } from "framer-motion";
+import Lottie from "lottie-react";
+import animationData from './animation.json';
+import styled from 'styled-components';
 
 
 function Item(props) {
-    
-    
     return (
         <Reorder.Item value={props.item} id={props.item} >
             <div className="result__item flex-row" onClick={(string) => props.handlePopularCities(props.item)}>
-                <p className="result__text">{props.item}</p>
+                <div className="result__liked flex-row">
+                    <p className="result__text">{props.item}</p>
+                </div>
                 <ArrowIosForwardOutline size="24px" className="result__icon" />
             </div>
         </Reorder.Item>
