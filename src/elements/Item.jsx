8169e74e3094f999951a9136603eccbce8d6c3d5
hitmaizer <jose.sousalves@gmail.com>
2022-02-25 16:01:48 +0000

    item element for popular cities

---
+++ src/elements/Item.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ArrowIosForwardOutline } from '@styled-icons/evaicons-outline/ArrowIosForwardOutline'
+import { useMotionValue, Reorder } from "framer-motion";
+
+
+function Item(props) {
+    
+    
+    return (
+        <Reorder.Item value={props.item} id={props.item} >
+            <div className="result__item flex-row" onClick={(string) => props.handlePopularCities(props.item)}>
+                <p className="result__text">{props.item}</p>
+                <ArrowIosForwardOutline size="24px" className="result__icon" />
+            </div>
+        </Reorder.Item>
+        
+    )
+}
+
+export default Item
