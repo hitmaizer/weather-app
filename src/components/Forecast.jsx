dc1cf0da19673d5d95ec18ba5bff3294d4c05106
hitmaizer <jose.sousalves@gmail.com>
2022-02-25 12:01:39 +0000

    deleted unused code + cleanup

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -9,48 +9,12 @@ export default function Forecast(props) {
 
 
     const forecastElements = forecastData[0].map(item => {
-        console.log(item)
         let m = moment(item.date)
-        /* let weatherImg = ""
-        switch(item.weather_state_abbr){
-                default:
-                break
-                case "sn":
-                    weatherImg = props.theme.images.snow
-                break
-                case "sl":
-                    weatherImg = props.theme.images.sleet
-                break
-                case "h":
-                    weatherImg = props.theme.images.hail
-                break
-                case "t": 
-                    weatherImg = props.theme.images.thunderStorm
-                break
-                case "hr":
-                    weatherImg = props.theme.images.heavyRain
-                break
-                case "lr":
-                    weatherImg = props.theme.images.lightRain
-                break
-                case "s":
-                    weatherImg = props.theme.images.shower
-                break
-                case "hc":
-                    weatherImg = props.theme.images.heavyCloud
-                break
-                case "lc":
-                    weatherImg = props.theme.images.lightCloud
-                break
-                case "c": 
-                    weatherImg = props.theme.images.clear
-                break            
-        } */
+        
         return (
             <DailyWeatherCard 
             key={nanoid()}
             details={item}
-            // weatherImg={weatherImg}
             tempFormat={props.tempFormat}
             date={m.format("dddd")}
             />
